c90558b51f40a147c8bf36a95ad98efc17c70c4e
Mahadi001 <sadat.himel@gmail.com>
2021-11-10 22:42:34 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -12,8 +12,15 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("hello");
-    if(!name){
-      
+    if (!name) {
+      //display alert
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName('');
     }
   };
 
@@ -35,11 +42,12 @@ function App() {
           </button>
         </div>
       </form>
+      {list.length > 0 && (  
       <div>
-        <List />
-
+        <List items={list} />
         <button className="clear-btn"> clear items</button>
       </div>
+      )}
     </section>
   );
 }
