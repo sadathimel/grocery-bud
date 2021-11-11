a70d949799f413216d7cf98a4eff0c0d3e804141
Mahadi001 <sadat.himel@gmail.com>
2021-11-11 17:49:18 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Alert from "./Alert";
 import List from "./List";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  }else{
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -59,6 +68,10 @@ function App() {
     setEditID(id)
     setName(specificItem.title);
   };
+
+  useEffect(()=>{
+    localStorage.setItem('list', JSON.stringify(list))
+  },[list])
 
   return (
     <section className="section-center">
